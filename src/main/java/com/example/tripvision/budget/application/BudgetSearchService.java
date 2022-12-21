ab790577aa2d4e52aa6c54741237b49f993bed9a
bryan0730 <52648542+bryan0730@users.noreply.github.com>
2022-12-21 17:09:08 +0900

    <refact> : <Budget URL 변경에 따른 로직 리팩토링> (#8)
    * <feat> : <프로젝트 전체 도메인 등록>
    
    - 프로젝트 전체 도메인 등록 (MEMBER, Project....)
    - 스프링 시큐리티 Oauth2 등록. (JWT 기반)
    
    * <feat> : <도메인 JSON 타입 추가>
    - 도메인 POST 테스트를 위한 JSON 파일 생성코드 등록
    
    
    
    <refact> : <Budget,File,Setting Domain 수정 및 Budget 관련 api 리팩토링>
    
    -Budget entity, dto, api 리팩토링, 관련 Project Domain 수정
    -File entity, dto 리팩토링
    -Setting entity, dto 리팩토링
    
    
    
    
    <refact> : <Budget findAll page처리>
    
    -Pageable 받지 못하는 문제로 SwaggerConfig.java에 코드 추가
    -Project Entity 연관관계 fetchType Lazy로 변경
    -Page Querydsl 메소드 추가
    
    * <refact> : <Budget URL 변경에 따른 로직 리팩토링>
    -Budget 도메인 검증 로직
    -BudgetController url 변경
    -BudgetDto inner class request dto 코드 추가
    -Service 파라미터 및 로직 변경
    
    * Revert "<refact> : <Budget URL 변경에 따른 로직 리팩토링>"
    This reverts commit c68675a50d24057521ca2a8e2b28c3b23f0f15a3.
    
    * <refact> : <Budget URL 변경에 따른 로직 리팩토링>""
    This reverts commit 1700c5c002535f24906ce5151928a946207b09b5.
    
    Co-authored-by: Lee Sang Min <ehdqn119@gmail.com>
    Co-authored-by: Lee Sang Min <59089706+ehdqn119@users.noreply.github.com>

--- src/main/java/com/example/tripvision/budget/application/BudgetSearchService.java
+++ src/main/java/com/example/tripvision/budget/application/BudgetSearchService.java
@@ -1,21 +1,17 @@
 package com.example.tripvision.budget.application;
 
-import com.example.tripvision.budget.dto.BudgetDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.example.tripvision.budget.domain.Budget;
 
 public interface BudgetSearchService {
-	public Page<Budget> searchBudget(Pageable pageable, String title);
-
-	public Page<Budget> findAll(Pageable pageable);
-
-	public void deleteAll();
-	public Budget findBudget(Long id);
-
-	public Budget saveBudget(Budget budget);
-	public Budget updateBudget(Long budgetId, Budget budget);
-	public void deleteBudget(Long budgetId);
+	Page<Budget> searchBudget(Pageable pageable, String title);
+	Page<Budget> findAll(Pageable pageable);
+	void deleteAll();
+	Budget findBudget(Long projectId, Long budgetId);
+	Budget saveBudget(Budget budget);
+	Budget updateBudget(Long budgetId, Budget budget);
+	void deleteBudget(Long projectId, Long budgetId);
 
 }
--- src/main/java/com/example/tripvision/budget/mapper/BudgetHelper.java
+++ src/main/java/com/example/tripvision/budget/mapper/BudgetHelper.java
@@ -19,10 +19,4 @@ public class BudgetHelper {
 			.build();
 	}
 
-	public static BudgetDto.Abc toAbcDto(Budget budget){
-		return BudgetDto.Abc.builder()
-			.note(budget.getNotes())
-			.value(budget.getValue())
-			.build();
-	}
 }
