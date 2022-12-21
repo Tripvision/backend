d39a69115aa9af92f63777b8a580583f14b403bb
uihoon <ehdgk9166@naver.com>
2022-12-21 12:26:50 +0900

    <refact> : <Budget URL 변경에 따른 로직 리팩토링>""
    This reverts commit 1700c5c002535f24906ce5151928a946207b09b5.

--- src/main/java/com/example/tripvision/budget/application/BudgetSearchServiceImpl.java
+++ src/main/java/com/example/tripvision/budget/application/BudgetSearchServiceImpl.java
@@ -1,10 +1,8 @@
 package com.example.tripvision.budget.application;
 
 
-
-import com.example.tripvision.budget.dto.BudgetDto;
 import com.example.tripvision.project.dao.ProjectRepository;
-import com.example.tripvision.project.dao.ProjectRepositoryImpl;
+
 import com.example.tripvision.project.domain.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.tripvision.budget.dao.BudgetRepository;
 import com.example.tripvision.budget.domain.Budget;
-import com.example.tripvision.file.domain.File;
 
 import lombok.RequiredArgsConstructor;
 
@@ -41,15 +38,18 @@ public class BudgetSearchServiceImpl implements BudgetSearchService {
 		budgetRepository.deleteAll();
 	}
 
-
 	/*
 	단일 조회 완
 	 */
 	@Transactional(readOnly = true)
-	public Budget findBudget(Long id) {
-		return budgetRepository.findById(id).orElseThrow(() -> new RuntimeException("해당 프로젝트가 없습니다."));
-	}
+	public Budget findBudget(Long projectId, Long budgetId) {
 
+		Budget budget = budgetRepository.findById(budgetId).orElseThrow(() -> new RuntimeException("not found budget"));
+
+		checkRelationProject(projectId, budget);
+
+		return budget;
+	}
 	/*
 	등록 완
 	 */
@@ -72,8 +72,12 @@ public class BudgetSearchServiceImpl implements BudgetSearchService {
 	 */
 	@Transactional
 	public Budget updateBudget(Long budgetId, Budget budget) {
+
 		Budget newBudget = budgetRepository.findById(budgetId)
-			.orElseThrow(() -> new RuntimeException("good"));
+			.orElseThrow(() -> new RuntimeException("not found Budget"));
+
+		checkRelationProject(budget.getProject().getId(), newBudget);
+
 		newBudget.update(budget);
 		return newBudget;
 	}
@@ -82,13 +86,22 @@ public class BudgetSearchServiceImpl implements BudgetSearchService {
 	삭제 완
 	 */
 	@Transactional
-	public void deleteBudget(Long budgetId) {
+	public void deleteBudget(Long projectId, Long budgetId) {
 
-		Budget budget = budgetRepository.findById(budgetId).orElseThrow(() -> new RuntimeException("exception ! "));
+		Budget budget = budgetRepository.findById(budgetId).orElseThrow(() -> new RuntimeException("budget not found"));
+
+		checkRelationProject(projectId, budget);
+
 		Project project = budget.getProject();
 		project.updateBudget(null);
 
 		budgetRepository.deleteById(budgetId);
 		// 단방향 이면, 따로 연관관계를 삭제해줍시다.
+	}
+
+	private void checkRelationProject(Long projectId, Budget budget) {
+		if (!budget.isMatchProjectId(projectId)){
+			throw new RuntimeException("project id not found");
+		}
 	}
 }
